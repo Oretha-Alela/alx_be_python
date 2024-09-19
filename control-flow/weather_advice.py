c93bf917c08be1e8a552c1a9920916371663b318
Consolata <consolatawanjiru45@gmail.com>
2024-09-19 16:29:37 +0300

    Fix bug in file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -5,15 +5,15 @@ weather = input("What's the weather like today? (sunny/rainy/cold): ")
 # Give recommendations
 
 if weather == "sunny":
-    recommendation = "Wear a t-shirt and sunglasses."
+    recommend = "Wear a t-shirt and sunglasses."
 elif weather == "rainy":
-    recommendation = "Don't forget your umbrella and a raincoat."
+    recommend = "Don't forget your umbrella and a raincoat."
 elif weather == "cold":
-    recommendation = "Make sure to wear a warm coat and a scarf."
+    recommend = "Make sure to wear a warm coat and a scarf."
 else:
-    recommendation = "Sorry, I don't have recommendations for this weather."
+    recommend = "Sorry, I don't have recommendations for this weather."
 
 # Print the recommendation
-print(recommendation)
+print(recommend)
  
 
