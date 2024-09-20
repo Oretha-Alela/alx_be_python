1ab8f74d630d9d20f9b85a7de0b43e1fffaa730b
Consolata <consolatawanjiru45@gmail.com>
2024-09-20 13:38:01 +0300

    Add Files

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,27 @@
+# Ask for user input"
+
+task = input("Enter a task description: ")
+
+priority = input("Enter the tasks priority (high, medium, low): ")
+
+time_bound = input("Is the task time-bound? (Yes or No): ")
+
+# React differently bassed on priority
+
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
+        else:
+            print(f"Reminder: {task} is a high priority task. Consider completing it when done with time-bound tasks")
+    case "medium":
+        if time_bound == "yes":
+            print(f"Reminder: {task} is a medium priority task that requires attention in due time")
+        else:
+            print(f"Reminder: {task} is a medium priority task. Consider completing it when done with high priority tasks")
+    case "low":
+        if time_bound == "yes":
+            print(f"Reminder: {task} is a low priority task.Consider doing it when the time comes.")
+        else:
+            print(f"{task} is a low priority task. Consider completing it when you have free time.")
+
