8f941dad1a4cf554f3bdab20b0e79386137b3b6a
Consolata <consolatawanjiru45@gmail.com>
2024-09-20 13:46:35 +0300

    Add Files

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,9 +2,9 @@
 
 task = input("Enter a task description: ")
 
-priority = input("Enter the tasks priority (high/medium/low): ")
+priority = input("Priority (high/medium/low): ")
 
-time_bound = input("Is the task time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 # React differently bassed on priority
 
